454dbe97468b9f0d55a15ba7b4ce5f7b1ee61cd3
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-15 19:05:00 +0530

    Complete archived_settings UI

--- lib/pages/archived_settings.dart
+++ lib/pages/archived_settings.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:ui_flutter_whatsapp/common/appbar.dart';
+import 'package:ui_flutter_whatsapp/common/chat_tile.dart';
+import 'package:ui_flutter_whatsapp/constants.dart';
+
+import '../common/list_tile.dart';
+
+const String info =
+    'Archived chats will remain archived when you receive a new message';
 
 class ArchiveSettingsPage extends StatelessWidget {
   const ArchiveSettingsPage({super.key});
@@ -8,6 +15,19 @@ class ArchiveSettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: 'Archive Settings'),
+      body: Column(
+        children: [
+          CustomListTile(
+            title: 'Keep chats archived',
+            subTitle: info,
+            trailingWidget: Switch(
+              activeColor: kAccentColor,
+              value: true,
+              onChanged: (p0) {},
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
