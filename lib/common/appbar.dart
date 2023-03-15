67c86b17f12dbc2fb8f3ebb2d7006323eb8cc883
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-16 02:25:43 +0530

    Update appbar, added customization in leading widget, actions..

--- lib/common/appbar.dart
+++ lib/common/appbar.dart
@@ -1,24 +1,73 @@
+// ignore_for_file: unnecessary_const, prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
-  CustomAppBar({
+import '../constants.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
     super.key,
+    required this.isChildWidget,
+    this.imageUrl,
+    this.leadingWidth,
     required this.title,
     this.actions,
   });
 
+  final bool isChildWidget;
+  final String? imageUrl;
+  final double? leadingWidth;
   final String title;
-  final List<Widget>? actions;
+  final Widget? actions;
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      elevation: 0.0,
-      title: Text(title),
-      actions: actions ?? [],
+    return Container(
+      height: 90,
+      width: double.infinity,
+      color: kAppBarColor,
+      child: Column(
+        children: [
+          const SizedBox(height: 45.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  isChildWidget
+                      ? GestureDetector(
+                          onTap: () => Navigator.pop(context),
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 10.0, right: 5.0),
+                            child: Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                      : const SizedBox.shrink(),
+                  imageUrl != null
+                      ? CircleAvatar(
+                          radius: 18.0,
+                          backgroundImage: AssetImage(imageUrl!),
+                        )
+                      : const SizedBox.shrink(),
+                  SizedBox(width: 12.0),
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  )
+                ],
+              ),
+              actions ?? const SizedBox.shrink(),
+            ],
+          )
+        ],
+      ),
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(60);
 }
