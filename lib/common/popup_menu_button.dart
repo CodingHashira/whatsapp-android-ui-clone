6f8ee7ec4ef8fa1a1b5a93e3deb0a2f15d180d2c
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-16 02:31:29 +0530

    Enhance popup_menu_button, supports actions mapping

--- lib/common/popup_menu_button.dart
+++ lib/common/popup_menu_button.dart
@@ -1,36 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:ui_flutter_whatsapp/model/data.dart';
 
 import '../constants.dart';
-import 'popup_menu_item.dart';
+
+final data = Data();
 
 class CustomPopupMenuButton extends StatelessWidget {
   const CustomPopupMenuButton({
     super.key,
+    this.popButtonColor,
+    // required this.popupMenuItems,
   });
+
+  final Color? popButtonColor;
+  // final Map<String, dynamic> popupMenuItems;
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
     return PopupMenuButton(
-      elevation: 0.0,
-      constraints: const BoxConstraints(minWidth: 190),
       color: kPopupMenuColor,
-      icon: const Icon(
+      child: Icon(
         Icons.more_vert_rounded,
-        color: kSecondaryColor,
+        color: popButtonColor ?? kSecondaryColor,
       ),
       itemBuilder: (context) => [
-        PopupMenuItem(
-          height: 32,
-          child: CustomPopupMenuItem(
-            title: 'Archive Settings',
-            onTap: () => Future(
-              () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, '/archiveSettingsPage');
-              },
-            ),
-          ),
-        ),
+        for (var item in data.chatPopupMenuItems.entries)
+          // for (var item in popupMenuItems.entries) // TODO to pass value from cons
+          PopupMenuItem(
+            padding: EdgeInsets.zero,
+            child: item.value is Map
+                ? Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.only(left: 18.0),
+                    child: PopupMenuButton(
+                        color: kPopupMenuColor,
+                        offset: Offset(width, -height),
+                        onOpened: () => Navigator.pop(context),
+                        child: Text(
+                          item.key,
+                          style: const TextStyle(
+                            color: kTextPrimaryColor,
+                          ),
+                        ),
+                        itemBuilder: (context) => [
+                              for (var x in Map.from(item.value).entries)
+                                PopupMenuItem(
+                                  child: Text(
+                                    x.key,
+                                    style: const TextStyle(
+                                      color: kTextPrimaryColor,
+                                    ),
+                                  ),
+                                ),
+                            ]),
+                  )
+                : PopupMenuItem(
+                    onTap: () {
+                      print('pressed');
+                    },
+                    child: Text(
+                      item.key,
+                      style: const TextStyle(
+                        color: kTextPrimaryColor,
+                      ),
+                    ),
+                  ),
+          )
       ],
     );
   }
