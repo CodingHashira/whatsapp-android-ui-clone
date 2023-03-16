73aedf427b82686b18162ceb825e886da2d16c64
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-17 02:33:23 +0530

    Refactor list_tile.dart

--- lib/common/list_tile.dart
+++ lib/common/list_tile.dart
@@ -4,64 +4,85 @@ import 'package:ui_flutter_whatsapp/constants.dart';
 class CustomListTile extends StatelessWidget {
   const CustomListTile({
     Key? key,
+    this.padding,
     this.leading,
+    this.leadingWidth,
+    this.contentPadding,
+    this.titleSize,
     required this.title,
     this.trailingWidget,
-    required this.subTitle,
+    this.subTitleSize,
+    this.subTitle,
   }) : super(key: key);
 
+  final EdgeInsetsGeometry? padding;
   final Widget? leading;
+  final double? leadingWidth;
+  final double? contentPadding;
+  final double? titleSize;
   final String title;
   final Widget? trailingWidget;
-  final String subTitle;
+  final double? subTitleSize;
+  final String? subTitle;
 
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Row(
-        children: [
-          if (leading != null)
-            Padding(
-              padding: const EdgeInsets.only(right: 12.0),
-              child: leading,
-            ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      title,
-                      style: kTitleTextStyle.copyWith(
-                        fontWeight: FontWeight.normal,
+    return GestureDetector(
+      child: Padding(
+        padding: padding ?? const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            if (leading != null)
+              Padding(
+                padding: const EdgeInsets.only(right: 12.0),
+                child: leading,
+              ),
+            SizedBox(width: leadingWidth ?? 0),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        title,
+                        style: kTitleTextStyle.copyWith(
+                          fontWeight: FontWeight.normal,
+                          fontSize: titleSize,
+                        ),
                       ),
-                    ),
-                    if (trailingWidget != null)
-                      SizedBox(
-                        height: 0.0,
-                        child: trailingWidget,
-                      ),
-                  ],
-                ),
-                const SizedBox(height: 5.0),
-                SizedBox(
-                  width:
-                      leading == null ? screenWidth - 120 : screenWidth - 120,
-                  child: Text(
-                    subTitle,
-                    style: kSubTitleTextStyle,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
+                      if (trailingWidget != null)
+                        SizedBox(
+                          height: 0.0,
+                          child: trailingWidget,
+                        ),
+                    ],
                   ),
-                )
-              ],
-            ),
-          )
-        ],
+                  SizedBox(height: contentPadding),
+                  if (subTitle != null)
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5),
+                      child: SizedBox(
+                        width: leading == null
+                            ? screenWidth - 120
+                            : screenWidth - 120,
+                        child: Text(
+                          subTitle!,
+                          style: kSubTitleTextStyle.copyWith(
+                            fontSize: subTitleSize,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 4,
+                        ),
+                      ),
+                    )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
