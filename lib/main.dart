bc53af7647474826dea7f0214cf645e39ae70fe1
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-17 02:44:28 +0530

    Refactor main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:ui_flutter_whatsapp/pages/settings.dart';
+import 'package:ui_flutter_whatsapp/widgets/chat_page/chat_screen.dart';
 import 'package:ui_flutter_whatsapp/constants.dart';
 import 'package:ui_flutter_whatsapp/pages/archived.dart';
+import 'package:ui_flutter_whatsapp/pages/testp.dart';
+import 'package:ui_flutter_whatsapp/widgets/settings_page/account.dart';
+import 'package:ui_flutter_whatsapp/widgets/settings_page/privacy.dart';
 
-import 'pages/archived_settings.dart';
+import 'pages/archive_settings.dart';
 import 'pages/launch.dart';
 
 void main() {
@@ -21,18 +26,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: kPrimaryColor,
-        appBarTheme: const AppBarTheme(backgroundColor: kAppBarColor),
-        cardColor: Colors.red,
-      ),
-      initialRoute: '/',
+      theme: kThemeData,
+      initialRoute: '/archivedPage',
       routes: {
         '/': (context) => const LaunchPage(),
         '/archivedPage': (context) => const ArchivedPage(),
         '/archiveSettingsPage': (context) => const ArchiveSettingsPage(),
+        '/chatScreen': (context) => const ChatScreen(),
+        '/testPage': (context) => const TestpPage(),
+        '/settingsPage': (context) => const SettingsPage(),
+        '/accountPage': (context) => const AccountPage(),
+        '/privacyPage': (context) => const PrivacyPage(),
       },
-      // home: const LaunchPage(),
     );
   }
 }
