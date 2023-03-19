92ab5b4023c722c1e9129dde15bceb38f430f4f2
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-19 15:52:02 +0530

    Modify appbar.dart, replaced custom with AppBar

--- lib/common/appbar.dart
+++ lib/common/appbar.dart
@@ -1,76 +1,63 @@
 import 'package:flutter/material.dart';
 
-import '../constants.dart';
-
-class CustomAppBar extends StatelessWidget {
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({
     Key? key,
     required this.isChildWidget,
-    this.imageUrl,
-    this.leadingWidth,
     required this.title,
+    this.imageUrl,
     this.actions,
   }) : super(key: key);
 
   final bool isChildWidget;
-  final String? imageUrl;
-  final double? leadingWidth;
   final String title;
+  final String? imageUrl;
   final Widget? actions;
 
   @override
+  Size get preferredSize => const Size.fromHeight(0);
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 90,
-      width: double.infinity,
-      color: kAppBarColor,
-      child: Column(
-        children: [
-          imageUrl != null
-              ? const SizedBox(height: 40.0)
-              : const SizedBox(height: 50.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  if (isChildWidget)
-                    GestureDetector(
-                      behavior: HitTestBehavior.translucent,
-                      onTap: () => Navigator.pop(context),
-                      child: const Padding(
-                        padding: EdgeInsets.only(left: 15.0, right: 5.0),
-                        child: Icon(
-                          Icons.arrow_back,
-                          color: Colors.white,
-                        ),
-                      ),
-                    )
-                  else
-                    const SizedBox.shrink(),
-                  if (imageUrl != null)
-                    CircleAvatar(
-                      radius: 18.0,
-                      backgroundImage: AssetImage(imageUrl!),
-                    )
-                  else
-                    const SizedBox.shrink(),
-                  const SizedBox(width: 20.0),
-                  Text(
-                    title,
-                    style: const TextStyle(
+    return AppBar(
+      titleSpacing: 8,
+      automaticallyImplyLeading: true,
+      elevation: 0.0,
+      leadingWidth: imageUrl != null ? 70 : 50,
+      leading: isChildWidget
+          ? GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 5.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min, //!
+                  children: [
+                    const Icon(
+                      Icons.arrow_back,
                       color: Colors.white,
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.w500,
+                      size: 28,
                     ),
-                  ),
-                ],
+                    imageUrl != null
+                        ? CircleAvatar(
+                            radius: 18.0,
+                            backgroundImage: AssetImage(imageUrl!),
+                          )
+                        : const SizedBox.shrink(),
+                  ],
+                ),
               ),
-              actions ?? const SizedBox.shrink(),
-            ],
-          ),
-        ],
+            )
+          : null,
+      title: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 18.0,
+          fontWeight: FontWeight.w500,
+        ),
       ),
+      actions: actions != null ? [actions!] : null,
+      centerTitle: false,
     );
   }
 }
