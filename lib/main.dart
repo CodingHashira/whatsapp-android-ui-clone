82be1690c56292604a7b1c2a6df5f820719280a9
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-19 16:02:57 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: kThemeData,
-      initialRoute: '/settingsPage',
+      initialRoute: '/chatScreen',
       routes: {
         '/': (context) => const LaunchPage(),
         '/archivedPage': (context) => const ArchivedPage(),
