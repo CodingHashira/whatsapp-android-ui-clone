609450eb9ed118a68d6cd04ed9fe210a61d7ac29
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-19 15:53:14 +0530

    Modify list_builder.dart, method updated

--- lib/common/list_builder.dart
+++ lib/common/list_builder.dart
@@ -5,12 +5,16 @@ import 'package:ui_flutter_whatsapp/widgets/chat_page/chat_bubble.dart';
 import 'chat_tile.dart';
 import 'list_tile.dart';
 
+var initIndex = 0;
+var previousSender = '';
+var verticalGap = 5.0;
+
 class CustomListBuilder extends StatelessWidget {
   const CustomListBuilder({
     super.key,
     required this.itemCount,
     required this.list,
-    required this.startIndex,
+    this.startIndex,
     this.leadingWidth,
     required this.returnWidgetType,
     this.padding,
@@ -21,7 +25,7 @@ class CustomListBuilder extends StatelessWidget {
 
   final int itemCount;
   final List list;
-  final int startIndex;
+  final int? startIndex;
   final double? leadingWidth;
   final Type returnWidgetType;
   final EdgeInsetsGeometry? padding;
@@ -37,7 +41,7 @@ class CustomListBuilder extends StatelessWidget {
       padding: padding ?? const EdgeInsets.only(top: 0.0),
       physics: const NeverScrollableScrollPhysics(),
       itemBuilder: (context, index) {
-        if (returnWidgetType == ChatBubble) {
+        if (returnWidgetType == CustomChatBubble) {
           return _buildChatBubble(index);
         } else if (returnWidgetType == ChatTile) {
           return _buildChatTile(index);
@@ -58,12 +62,14 @@ class CustomListBuilder extends StatelessWidget {
   }
 
   void handleNavigation(BuildContext context, int index) {
-    var pageRoute = list[index + startIndex]['pageRoute'];
+    if (startIndex != null) initIndex = startIndex!;
+
+    var pageRoute = list[index]['pageRoute'];
     if (pageRoute != null) {
       Navigator.pushNamed(context, pageRoute);
     }
 
-    var dialogWidget = list[index + startIndex]['dialogWidget'];
+    var dialogWidget = list[index]['dialogWidget'];
     if (dialogWidget != null) {
       showDialog(
         context: context,
@@ -75,25 +81,38 @@ class CustomListBuilder extends StatelessWidget {
   }
 
   CustomListTile _buildCustomListTile(int index, BuildContext context) {
+    if (startIndex != null) initIndex = startIndex!;
+
     return CustomListTile(
       onTap: () => handleNavigation(context, index),
       padding: listTilePadding ??
           const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
-      leading: list[index + startIndex]['leading'],
+      leading: list[index + initIndex]['leading'],
       leadingWidth: leadingWidth,
-      title: list[index + startIndex]['title'],
+      title: list[index + initIndex]['title'],
       titleColor: titleColor,
       titleSize: titleSize,
-      subTitle: list[index + startIndex]['subTitle'],
-      trailingWidget: list[index + startIndex]['trailing'],
+      subTitle: list[index + initIndex]['subTitle'],
+      trailingWidget: list[index + initIndex]['trailing'],
     );
   }
 
-  ChatBubble _buildChatBubble(int index) {
-    return ChatBubble(
-      text: list[index]['text'].trim(),
+  CustomChatBubble _buildChatBubble(int index) {
+    if (previousSender == '') {
+      previousSender = list[index]['sender'];
+    }
+
+    if (previousSender == list[index]['sender']) {
+      verticalGap = 2;
+    } else {
+      verticalGap = 8.0;
+    }
+
+    return CustomChatBubble(
+      verticalGap: verticalGap,
+      text: list[index]['text'],
       isMe: list[index]['sender'] == 'me',
-      textLength: list[index]['text'].length,
+      time: '12:20',
     );
   }
 
@@ -101,7 +120,7 @@ class CustomListBuilder extends StatelessWidget {
     return ChatTile(
       leading: CircleAvatar(
         radius: 23.0,
-        backgroundColor: Color.fromARGB(255, 78, 95, 105),
+        backgroundColor: const Color.fromARGB(255, 78, 95, 105),
         backgroundImage: AssetImage(
           list[index].imageUrl,
         ),
