de03864161470d7decd2dae1c16983e6ddd1cf0f
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-22 16:19:00 +0530

    Modify choose_country.dart

--- lib/pages/settings_page/account_page/choose_country.dart
+++ lib/pages/settings_page/account_page/choose_country.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:ui_flutter_whatsapp/constants.dart';
+
+import 'package:ui_flutter_whatsapp/model/data.dart';
 import 'package:ui_flutter_whatsapp/common/appbar.dart';
+import 'package:ui_flutter_whatsapp/common/list_builder.dart';
+import 'package:ui_flutter_whatsapp/widgets/country_code_tile.dart';
+
+const data = Data();
 
 class ChooseCountry extends StatelessWidget {
   const ChooseCountry({super.key});
@@ -7,13 +14,26 @@ class ChooseCountry extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
+      appBar: const CustomAppBar(
         isChildWidget: true,
         title: 'Choose a country',
-        actions: Icon(Icons.search),
+        actions: Padding(
+          padding: EdgeInsets.only(right: 15.0),
+          child: Icon(
+            Icons.search,
+            color: kSecondaryColor,
+          ),
+        ),
       ),
-      body: Column(
-        children: [],
+      body: ListView(
+        children: [
+          CustomListBuilder(
+            startIndex: 0,
+            itemCount: data.countryCodesList.length,
+            list: data.countryCodesList,
+            returnWidgetType: CountryCodeTile,
+          ),
+        ],
       ),
     );
   }
