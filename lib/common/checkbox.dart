7c6e19ddb9aebb64a7a27f50a0e292840fe7e643
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-22 16:17:07 +0530

    Modify checkbox.dart, added label

--- lib/common/checkbox.dart
+++ lib/common/checkbox.dart
@@ -2,26 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:ui_flutter_whatsapp/constants.dart';
 
 class CustomCheckBox extends StatefulWidget {
-  const CustomCheckBox({super.key});
+  const CustomCheckBox({
+    super.key,
+    this.verticalDensity,
+    this.label,
+    this.style,
+  });
+
+  final double? verticalDensity;
+  final String? label;
+  final TextStyle? style;
 
   @override
   State<CustomCheckBox> createState() => _CustomCheckBoxState();
 }
 
 class _CustomCheckBoxState extends State<CustomCheckBox> {
-  bool checkValue = true;
+  bool checkValue = false;
 
   @override
   Widget build(BuildContext context) {
-    return Checkbox(
-      materialTapTargetSize: MaterialTapTargetSize.padded,
-      visualDensity: const VisualDensity(vertical: -4),
-      value: checkValue,
-      activeColor: kAccentColor,
-      checkColor: kPrimaryColor,
-      onChanged: (p0) => setState(() {
-        checkValue = p0!;
-      }),
+    return Row(
+      children: [
+        Checkbox(
+          materialTapTargetSize: MaterialTapTargetSize.padded,
+          visualDensity:
+              VisualDensity(vertical: widget.verticalDensity ?? -4.0),
+          value: checkValue,
+          activeColor: kAccentColor,
+          checkColor: kPrimaryColor,
+          onChanged: (p0) => setState(() {
+            checkValue = p0!;
+          }),
+        ),
+        widget.label != null
+            ? Expanded(
+                child: Text(
+                  widget.label!,
+                  style: widget.style ?? kSubTitleTextStyle,
+                ),
+              )
+            : const SizedBox.shrink()
+      ],
     );
   }
 }
