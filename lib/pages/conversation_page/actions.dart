295033602053d2f549af4b36526e02fe678398f7
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-23 15:37:08 +0530

    Modify actions.dart, support call dialog

--- lib/pages/conversation_page/actions.dart
+++ lib/pages/conversation_page/actions.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:ui_flutter_whatsapp/alert_dialogs/conversation_call.dart';
 
 import 'package:ui_flutter_whatsapp/constants.dart';
 import 'package:ui_flutter_whatsapp/model/data.dart';
 import 'package:ui_flutter_whatsapp/common/popup_menu_button.dart';
+import 'package:ui_flutter_whatsapp/services/handle_navigation.dart';
 
 class ChatPageActions extends StatelessWidget {
   static const data = Data();
@@ -18,7 +20,11 @@ class ChatPageActions extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => NavigationHelper.openDialog(
+              context: context,
+              dialogWidget:
+                  const ConversationCallDialog(title: 'Start video call?'),
+            ),
             icon: const Icon(
               Icons.videocam_rounded,
               color: Colors.white,
@@ -26,7 +32,11 @@ class ChatPageActions extends StatelessWidget {
             ),
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: () => NavigationHelper.openDialog(
+              context: context,
+              dialogWidget:
+                  const ConversationCallDialog(title: 'Start voice call?'),
+            ),
             icon: const Icon(
               Icons.call_rounded,
               color: Colors.white,
