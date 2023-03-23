fd5d4e50fd6a9db82bc23565c12a098f84e77761
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-24 02:02:30 +0530

    Modify rich_text.dart, alignment, padding

--- lib/common/rich_text.dart
+++ lib/common/rich_text.dart
@@ -2,21 +2,29 @@ import 'package:flutter/material.dart';
 
 class CustomRichText extends StatelessWidget {
   const CustomRichText({
-    super.key,
+    Key? key,
     required this.textList,
-  });
+    this.textAlign = TextAlign.left,
+    this.padding,
+  }) : super(key: key);
 
   final List<Map<String, TextStyle>> textList;
+  final TextAlign textAlign;
+  final EdgeInsetsGeometry? padding;
 
   @override
   Widget build(BuildContext context) {
-    return Text.rich(
-      TextSpan(
-        children: [
-          for (var item in textList)
-            for (var textMap in item.entries)
-              TextSpan(text: textMap.key, style: textMap.value),
-        ],
+    return Padding(
+      padding: padding ?? const EdgeInsets.all(0.0),
+      child: Text.rich(
+        TextSpan(
+          children: [
+            for (var item in textList)
+              for (var textMap in item.entries)
+                TextSpan(text: textMap.key, style: textMap.value),
+          ],
+        ),
+        textAlign: textAlign,
       ),
     );
   }
