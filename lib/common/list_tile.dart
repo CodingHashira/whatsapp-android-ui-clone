619bf8b46875e2d8d47c2a8b5236cd74a9f0c300
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-28 16:18:40 +0530

    Modify list_tile.dart

--- lib/common/list_tile.dart
+++ lib/common/list_tile.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:ui_flutter_whatsapp/constants.dart';
-import 'package:ui_flutter_whatsapp/model/data.dart';
-import 'checkbox.dart';
-
-final double screenWidth = Data.screen.width;
 
 class CustomListTile extends StatelessWidget {
   const CustomListTile({
@@ -28,6 +24,7 @@ class CustomListTile extends StatelessWidget {
     this.leadingEndIndent,
     this.tileColor,
     this.wrapText,
+    this.titleWidget,
   });
 
   final VoidCallback? onTap;
@@ -36,8 +33,9 @@ class CustomListTile extends StatelessWidget {
   final Widget? leading;
   final double? leadingIndent;
   final double? leadingEndIndent;
-  final CrossAxisAlignment? leadingAlignment;
+  final MainAxisAlignment? leadingAlignment;
   final String? title;
+  final Widget? titleWidget;
   final TextStyle? titleStyle;
   final double? titleIndent;
   final String? subTitle;
@@ -58,100 +56,60 @@ class CustomListTile extends StatelessWidget {
     final defaultSubTitleStyle = kSubTitleTextStyle.copyWith(
       color: const Color(0xff3b4a55),
     );
-    final hasSubTitle = subTitle != null || subTitleWidget != null;
-    final hasLeading = leading != null;
-    final hasTrailing = trailing != null;
+    final screenWidth = MediaQuery.of(context).size.width;
 
-    return GestureDetector(
+    return ListTile(
+      tileColor: tileColor,
+      dense: leading is Icon ? true : false,
+      visualDensity: VisualDensity(vertical: leading == CircleAvatar ? -4 : 0),
       onTap: isEnabled != false ? onTap : null,
-      behavior: HitTestBehavior.translucent,
-      child: Container(
-        color: tileColor,
-        padding: padding ?? const EdgeInsets.fromLTRB(10.0, 15.0, 5.0, 15.0),
-        child: Stack(
-          alignment: trailingAlignment ?? Alignment.topRight,
-          children: [
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: leadingAlignment ?? CrossAxisAlignment.center,
-              children: [
-                SizedBox(width: leadingIndent ?? 10.0),
-                if (hasLeading)
-                  Padding(
-                    padding: EdgeInsets.only(right: leadingEndIndent ?? 15.0),
-                    child: leading!,
-                  ),
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      title != null
-                          ? SizedBox(
-                              height: 23.0,
-                              width: screenWidth - (titleIndent ?? 90),
-                              child: Text(
-                                title!,
-                                style: titleStyle ??
-                                    (isEnabled ?? true
-                                        ? kTitleTextStyle
-                                        : defaultTitleStyle),
-                              ),
-                            )
-                          : const SizedBox.shrink(),
-                      hasSubTitle
-                          ? SizedBox(height: contentPadding ?? 0.0)
-                          : const SizedBox.shrink(),
-                      hasSubTitle
-                          ? SizedBox(
-                              width: screenWidth - (subTitleIndent ?? 0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  if (subTitle != null)
-                                    Padding(
-                                      padding: const EdgeInsets.only(top: 5.0),
-                                      child: Text(
-                                        subTitle!,
-                                        maxLines: wrapText == true ? 1 : null,
-                                        overflow: wrapText == true
-                                            ? TextOverflow.ellipsis
-                                            : null,
-                                        style: subTitleStyle ??
-                                            (isEnabled ?? true
-                                                ? kSubTitleTextStyle
-                                                : defaultSubTitleStyle),
-                                      ),
-                                    ),
-                                  subTitleWidget != null
-                                      ? subTitleWidget!
-                                      : const SizedBox.shrink(),
-                                ],
-                              ),
-                            )
-                          : const SizedBox.shrink(),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            hasTrailing
-                ? Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      SizedBox(
-                        height: 20.0,
-                        child: trailing! is CustomCheckBox
-                            ? CustomCheckBox(isEnabled: isEnabled)
-                            : trailing!,
-                      ),
-                    ],
-                  )
-                : const SizedBox.shrink(),
-          ],
-        ),
-      ),
+      contentPadding: padding ?? const EdgeInsets.all(5.0),
+      minLeadingWidth: leadingEndIndent ?? 0.0,
+      leading: leading != null
+          ? Padding(
+              padding: EdgeInsets.only(left: leadingIndent ?? 15.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: leadingAlignment ?? MainAxisAlignment.center,
+                children: [
+                  leading!,
+                ],
+              ),
+            )
+          : const SizedBox.shrink(),
+      title: title != null
+          ? Text(
+              title!,
+              style: titleStyle ??
+                  (isEnabled ?? true ? kTitleTextStyle : defaultTitleStyle),
+            )
+          : titleWidget,
+      subtitle: subTitle != null
+          ? Container(
+              padding: EdgeInsets.only(top: contentPadding ?? 3.0),
+              width: screenWidth - (subTitleIndent ?? 0),
+              child: Text(
+                subTitle!,
+                style: subTitleStyle ??
+                    (isEnabled ?? true
+                        ? kSubTitleTextStyle
+                        : defaultSubTitleStyle),
+                maxLines: wrapText == true ? 1 : null,
+                overflow: wrapText == true ? TextOverflow.ellipsis : null,
+              ),
+            )
+          : subTitleWidget,
+      trailing: trailing != null
+          ? Padding(
+              padding: const EdgeInsets.only(right: 5.0),
+              child: Stack(
+                alignment: trailingAlignment ?? Alignment.topRight,
+                children: [
+                  trailing!,
+                ],
+              ),
+            )
+          : const SizedBox.shrink(),
     );
   }
 }
