76405219b029ba688c0e55a1884e0ad7d1faba06
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-03-28 16:24:39 +0530

    Modify network_tile.dart

--- lib/widgets/network_usage/network_tile.dart
+++ lib/widgets/network_usage/network_tile.dart
@@ -24,64 +24,65 @@ class NetworkTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomListTile(
+      trailingAlignment: Alignment.topRight,
+      subTitleIndent: 0,
+      leadingIndent: 8.0,
+      leadingEndIndent: 40.0,
+      padding: const EdgeInsets.fromLTRB(15.0, 10.0, .0, 10.0),
       leading: Icon(icon),
-      leadingIndent: 20.0,
-      leadingAlignment: CrossAxisAlignment.start,
-      padding: const EdgeInsets.fromLTRB(15.0, 12.0, 25.0, 12.0),
-      title: title,
+      titleWidget: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: kTitleTextStyle,
+          ),
+          Row(
+            // mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                dataConsumptionList[0],
+                style: kSubTitleTextStyle.copyWith(fontSize: 13.0),
+              ),
+              const ColorDot(
+                color: kSecondaryColor,
+                radius: 3.0,
+              ),
+              Text(
+                dataConsumptionList[1],
+                style: kSubTitleTextStyle.copyWith(fontSize: 13.0),
+              ),
+            ],
+          ),
+        ],
+      ),
       subTitleWidget: Column(
         children: [
           const SizedBox(height: 5.0),
           CustomLinearPercentIndicator(
             padding: const EdgeInsets.only(left: 0.0),
-            height: 3.2,
+            height: 4.0,
             percent: percent,
           ),
-          const SizedBox(height: 10.0),
-          subTitleList != null
-              ? Row(
-                  children: [
-                    Text(
-                      subTitleList![0],
-                      style: kSubTitleTextStyle.copyWith(fontSize: 13.0),
-                    ),
-                    const ColorDot(
-                      color: kSecondaryColor,
-                      radius: 3.0,
-                    ),
-                    Text(
-                      subTitleList![1],
-                      style: kSubTitleTextStyle.copyWith(fontSize: 13.0),
-                    ),
-                  ],
-                )
-              : const SizedBox.shrink(),
-        ],
-      ),
-      trailing: Row(
-        textBaseline: TextBaseline.alphabetic,
-        crossAxisAlignment: CrossAxisAlignment.baseline,
-        children: [
-          const Icon(
-            Icons.arrow_upward_rounded,
-            size: 13.0,
-          ),
-          Text(
-            dataConsumptionList[0],
-            style: kSubTitleTextStyle.copyWith(fontSize: 13.0),
-          ),
-          const SizedBox(width: 5.0),
-          const Icon(
-            Icons.arrow_downward_rounded,
-            size: 13.0,
-          ),
-          Text(
-            dataConsumptionList[1],
-            style: kSubTitleTextStyle.copyWith(fontSize: 13.0),
+          const SizedBox(height: 5.0),
+          Row(
+            children: [
+              Text(
+                subTitleList![0],
+                style: kSubTitleTextStyle.copyWith(fontSize: 13.0),
+              ),
+              const ColorDot(
+                color: kSecondaryColor,
+                radius: 3.0,
+              ),
+              Text(
+                subTitleList![1],
+                style: kSubTitleTextStyle.copyWith(fontSize: 13.0),
+              ),
+            ],
           ),
         ],
       ),
-      // trailingMaxWidth: 130.0, //TODO: remove unnecessary listTile prop
     );
   }
 }
